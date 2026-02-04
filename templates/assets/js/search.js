d36c8a0da682b51518cdd70871292f773915cc9c
atangccc <2199287121@qq.com>
2026-02-04 13:59:16 +0800

    Update theme

--- templates/assets/js/search.js
+++ templates/assets/js/search.js
@@ -1,8 +1,8 @@
 /**
  * Theme: theme-Serenity
  * Author: Serenity
- * Build: 2026-02-04 13:58:32
- * Fingerprint: 3df5e47e31189865
+ * Build: 2026-02-04 13:59:16
+ * Fingerprint: 214816a4241f3d34
  * Copyright © 2026 Serenity. All rights reserved.
  * Unauthorized copying or distribution is prohibited.
  */
